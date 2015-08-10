d4ea43700315ba6ff2ac3f7f5bb3f422b12130e1
root <root@puppet-test-master.cloud.csd.uwo.ca>
2015-08-10 15:13:20 -0400

    Initial import

---
+++ manifests/base.pp
@@ -0,0 +1,15 @@
+# == Class: profile
+#
+# Base profile for all nodes.
+#
+# === Authors
+#
+# Jeff Shantz <jeff@csd.uwo.ca>
+#
+# === Copyright
+#
+# Copyright 2015, Western University, unless otherwise noted.
+#
+class profile::base {
+  include ::base
+}
---
+++ manifests/init.pp
@@ -0,0 +1,14 @@
+# == Class: profile
+#
+# Profiles for Western Computer Science.
+#
+# === Authors
+#
+# Jeff Shantz <jeff@csd.uwo.ca>
+#
+# === Copyright
+#
+# Copyright 2015, Western University, unless otherwise noted.
+#
+class profile {
+}
